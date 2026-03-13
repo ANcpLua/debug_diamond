a6be9a90d77c293341cbff50faf6c9cd1b768b1a
ancplua <anfh22@outlook.com>
2026-03-13 20:10:28 +0100

    Initial commit

---
+++ debug/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+struct A { int x = 42; };
+
+struct B : virtual A {};
+struct C : virtual A {};
+struct D : B, C {};
+
+int main() {
+    D d;
+
+    A* a_from_b = static_cast<B*>(&d);
+    A* a_from_c = static_cast<C*>(&d);
+
+    std::cout << a_from_b << std::endl;
+    std::cout << a_from_c << std::endl;
+}
